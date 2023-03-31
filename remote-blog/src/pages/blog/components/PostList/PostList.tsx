b185f61a6f8d98076f400120e9ce67c01bf09190
Uyen Nguyen <uyenntt17407@st.uel.edu.vn>
2023-03-31 12:15:45 +0700

    Create Server and FetchAPI in RTK Query

--- remote-blog/src/pages/blog/components/PostList/PostList.tsx
+++ remote-blog/src/pages/blog/components/PostList/PostList.tsx
@@ -1,5 +1,5 @@
 import PostItem from '../PostItem'
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
 import { RootState, useAppDispatch } from '../../../../store'
 import { deletePost, getPostList, startEditingPost } from '../../blog.reducer'
 import { Fragment, useEffect } from 'react'
@@ -32,7 +32,7 @@ export default function PostList() {
     <div className='bg-white py-6 sm:py-8 lg:py-12'>
       <div className='mx-auto max-w-screen-xl px-4 md:px-8'>
         <div className='mb-10 md:mb-16'>
-          <h2 className='mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl'>Được Dev Blog</h2>
+          <h2 className='mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl'>Blog</h2>
           <p className='mx-auto max-w-screen-md text-center text-gray-500 md:text-lg'>
             Đừng bao giờ từ bỏ. Hôm nay khó khăn, ngày mai sẽ trở nên tồi tệ. Nhưng ngày mốt sẽ có nắng
           </p>
